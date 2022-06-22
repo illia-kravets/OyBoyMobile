3da6f07f23e35d3dfbb03a6655cdb3a01e2f57ba
illia-kravets <illia.kravets@datawiz.io>
2022-06-22 19:56:27 +0300

    player

--- lib/widgets/default/player.dart
+++ lib/widgets/default/player.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:oyboy/constants/export.dart';
 import 'package:oyboy/data/models/video.dart';
+import 'package:oyboy/utils/utils.dart';
 import 'package:video_player/video_player.dart';
 
 import 'loadingVideoBanner.dart';
@@ -40,7 +41,7 @@ class _AppVideoPlayerState extends State<AppVideoPlayer> {
     return Container(
       height: widget.height,
       width: widget.width,
-      child: _controller.value.isInitialized
+      child: _controller.value.isInitialized || widget.video.video == null || widget.video.video!.isEmpty
           ? Stack(
               children: [
                 Center(
@@ -53,7 +54,12 @@ class _AppVideoPlayerState extends State<AppVideoPlayer> {
                 )
               ],
             )
-          : LoadingVideoBanner(height: widget.height, url: widget.video.banner),
+          : Stack(
+            children: [
+              LoadingVideoBanner(height: widget.height, url: widget.video.banner),
+              const Loader(width: 30, height: 30, strokeWidth: 4,)
+            ],
+          ),
     );
   }
 
@@ -77,34 +83,91 @@ class VideoPlaybackController extends StatefulWidget {
 
 class _VideoPlaybackControllerState extends State<VideoPlaybackController> {
   bool visible = true;
+  bool inTransition = false;
 
   bool aheadScrollVisible = false;
-  bool backScrollVisible = true;
+  bool backScrollVisible = false;
 
-  void toggleVisibility() async {
-    setState(() => visible = true);
-    await Future.delayed(Duration(seconds: 1));
-    setState(() => visible = false);
+  Future _makeTransition(Duration duration) async {
+    inTransition = true;
+    await Future.delayed(duration);
+    inTransition = false;
+  }
+
+  void _setScrollVisibility(bool visibility, VideoScrollType scrollType) {
+    if (scrollType == VideoScrollType.ahead)
+      aheadScrollVisible = visibility;
+    else
+      backScrollVisible = visibility;
+  }
+
+  void _toggleVisibility() async {
+    setState(() => visible = !visible);
+  }
+
+  void _tapAction() async {
+    if (!widget.controller.value.isPlaying || visible) return _toggleVisibility();
+    _toggleVisibility();
+    if (inTransition) return;
+    await _makeTransition(const Duration(seconds: 2));
+    if (visible) _toggleVisibility();
+  }
+
+  void _pauseAction() async {
+    setState(() {});
+    if (!visible) return _toggleVisibility();
+    if (widget.controller.value.isPlaying) return widget.controller.pause();
+    widget.controller.play();
+    await _makeTransition(const Duration(seconds: 2));
+    _toggleVisibility();
+  }
+
+  void _scrollAction(VideoScrollType scrollType) async {
+    setState(() {
+      visible = true;
+      _setScrollVisibility(true, scrollType);
+    });
+    Duration videoPosition =
+        await widget.controller.position ?? const Duration();
+    switch (scrollType) {
+      case VideoScrollType.back:
+        widget.controller.seekTo(videoPosition - const Duration(seconds: 10));
+        break;
+      case VideoScrollType.ahead:
+        widget.controller.seekTo(videoPosition + const Duration(seconds: 10));
+        break;
+    }
+    await _makeTransition(const Duration(seconds: 1));
+    setState(() {
+      if(widget.controller.value.isPlaying) visible = false;
+      _setScrollVisibility(false, scrollType);
+    });
+  }
+
+  Future<IconData> getPlayingIcon() async {
+    if (await widget.controller.position == widget.controller.value.duration)
+      return Icons.replay;
+    if(widget.controller.value.isPlaying) return Icons.pause;
+    return Icons.play_arrow;
   }
 
   @override
   Widget build(BuildContext context) {
+    widget.controller;
+
     return AnimatedOpacity(
       duration: const Duration(milliseconds: 200),
       opacity: visible ? 1.0 : 0,
       child: GestureDetector(
-        onTap: () {},
+        onTap: () => _tapAction(),
         child: Container(
           color: Colors.black.withOpacity(0.2),
           child:
               Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
             _scrollWidget(VideoScrollType.back),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 100),
-              child: IconButton(
-                icon: _playerIcon(Icons.pause),
-                onPressed: () {},
-              ),
+            IconButton(
+              icon: _playerIcon(widget.controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+              onPressed: () => _pauseAction(),
             ),
             _scrollWidget(VideoScrollType.ahead)
           ]),
@@ -116,7 +179,8 @@ class _VideoPlaybackControllerState extends State<VideoPlaybackController> {
   Widget _playerIcon(IconData icon) => Center(
         child: Icon(
           icon,
-          size: 32,
+          size: 38,
+          color: Colors.white,
         ),
       );
 
@@ -126,7 +190,7 @@ class _VideoPlaybackControllerState extends State<VideoPlaybackController> {
         : aheadScrollVisible;
 
     return GestureDetector(
-      onDoubleTap: () => _scrollVideo(scrollType),
+      onDoubleTap: () => _scrollAction(scrollType),
       child: AnimatedOpacity(
           opacity: visible ? 1.0 : 0,
           duration: const Duration(milliseconds: 200),
@@ -134,7 +198,8 @@ class _VideoPlaybackControllerState extends State<VideoPlaybackController> {
             padding: const EdgeInsets.symmetric(vertical: 10),
             child: Row(
               children: [
-                _playerIcon(Icons.keyboard_double_arrow_right),
+                _playerIcon(scrollType == VideoScrollType.back ? 
+                  Icons.keyboard_double_arrow_left : Icons.keyboard_double_arrow_right),
                 const Text("s.")
               ],
             ),
@@ -142,32 +207,4 @@ class _VideoPlaybackControllerState extends State<VideoPlaybackController> {
     );
   }
 
-  void _setScrollVisibility(bool visibility, VideoScrollType scrollType) {
-    if (scrollType == VideoScrollType.ahead)
-      aheadScrollVisible = visibility;
-    else
-      backScrollVisible = visibility;
-  }
-
-  void _scrollVideo(VideoScrollType scrollType) async {
-    setState(() {
-      visible = true;
-      _setScrollVisibility(true, scrollType);
-    });
-    Duration videoPosition =
-        await widget.controller.position ?? const Duration();
-    switch (scrollType) {
-      case VideoScrollType.back:
-        widget.controller.seekTo(videoPosition + const Duration(seconds: 10));
-        break;
-      case VideoScrollType.ahead:
-        widget.controller.seekTo(videoPosition - const Duration(seconds: 10));
-        break;
-    }
-    await Future.delayed(const Duration(milliseconds: 500));
-    setState(() {
-      visible = false;
-      _setScrollVisibility(false, scrollType);
-    });
-  }
 }
